16955c5a274149afab03cc842a44a23601ba4b23
catalin <catalin.casuneanu@gmail.com>
2021-01-24 13:02:20 +0200

    first

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LandingScreen from "./components/auth/Landing";
+import RegisterScreen from "./components/auth/Register";
+import LoginScreen from "./components/auth/Login";
+import * as firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyCjlZxMJEKFeEAfJd5uurGciIpZ7cZsx8c",
+  authDomain: "socialite-b912d.firebaseapp.com",
+  projectId: "socialite-b912d",
+  storageBucket: "socialite-b912d.appspot.com",
+  messagingSenderId: "780876704868",
+  appId: "1:780876704868:web:8b518608bc5dd032018371",
+  measurementId: "G-2R3YF491CR",
+};
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
 }
+const Stack = createStackNavigator();
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    };
+  }
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        });
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        });
+      }
+    });
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Text>Loading</Text>
+        </View>
+      );
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen
+              name="Landing"
+              component={LandingScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <Text>User is logged in</Text>
+      </View>
+    );
+  }
+}
+export default App;
---
+++ components/auth/Login.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { View, Button, TextInput } from "react-native";
+import firebase from "firebase";
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      name: "",
+    };
+    this.onSignUp = this.onSignUp.bind(this);
+  }
+  onSignUp() {
+    const { email, password } = this.state;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <TextInput
+          placeholder="Email"
+          onChangeText={(email) => this.setState({ email })}
+        />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(password) => this.setState({ password })}
+        />
+        <Button onPress={() => this.onSignUp()} title="Sign In" />
+      </View>
+    );
+  }
+}
